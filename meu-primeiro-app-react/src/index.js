f30376ac4dcc4fb007a83073ca1753f9aaa528dd
Evandro Ferraz <ferraz.evandro@outlook.com>
2024-04-11 16:37:06 -0300

    Add files via upload

---
+++ meu-primeiro-app-react/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Meu primeiro app ReactJS</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
